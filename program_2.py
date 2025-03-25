9b9f6e500ba2a8affbd7b9dda1cb132d9eacd9e5
Alethea Lo <aylo@students.unwsp.edu>
2025-03-24 20:47:44 -0500

    Add files via upload
    #Programmer: Alethea Lo
    #Date: 3/24/25
    #Title: Random Number File Writer

--- program_2.py
+++ program_2.py
@@ -1,5 +1,24 @@
-# Program #2: Random Number File Writer
-# Write a program that writes a series of random numbers (up to 1000) to a file.
-# Each random number should be in the range of 1 through 500. 
-# The application should let the user specify how many random numbers the file will hold 
-# (up to 1000).
+#Programmer: Alethea Lo
+#Date: 3/24/25
+#Title: Random Number File Writer
+
+import random
+while True:
+    try:
+        count = int(input("Enter how many random numbers to generate (up to 1000): "))
+        if 1 <= count <= 1000:
+            break
+        else:
+            print("Please enter a number between 1 and 1000.")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+
+with open("random_numbers.txt", "w") as file:
+    for _ in range(count):
+
+        random_number = random.randint(1, 500)
+
+        file.write(f"{random_number}\n")
+
+print(f"{count} random numbers have been written to 'random_numbers.txt'.")
